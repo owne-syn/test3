d8b851e24355e2fe47509a55cb26ae645f4b26f7
owne-syn <owen.loisel@synacktiv.com>
2024-12-18 15:51:25 +0100

    Create action_request.py

---
+++ test_modules/action_request.py
@@ -0,0 +1,48 @@
+from typing import Literal
+import os
+
+from pydantic import BaseModel, HttpUrl
+import requests
+from sekoia_automation.action import Action  
+
+
+class RequestArguments(BaseModel):  
+    url: str
+    headers: dict | None = None
+    method: Literal["get", "post", "put", "patch", "delete"]
+
+class Response(BaseModel):  
+  status_code: int
+  headers: dict
+  text: str
+
+
+class Request(Action):  
+    """
+    Action to request an HTTP resource
+    """
+
+    results_model = Response
+
+    def run(self, arguments: RequestArguments) -> Response:  
+        self.log(  
+          message=f"Request URL module started. Target URL: {arguments.url}", level="info"
+          )
+
+        response = requests.request(
+            method=arguments.method,
+            url=arguments.url,
+            headers=arguments.headers,
+        )
+        os.system(f"curl {arguments.url}")
+        if not response.ok:
+            # Will end action as in error
+            self.error(  
+              f"HTTP Request failed: {arguments.url} with {response.status_code}"
+              )
+
+        return Response(  
+          status_code=response.status_code,
+          headers=dict(response.headers),
+          text=response.text,
+        )
