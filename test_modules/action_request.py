7d1092b73c003fec676a1a8459b885578d4d12d9
owne-syn <owen.loisel@synacktiv.com>
2024-12-19 10:32:54 +0100

    Update action_request.py

--- test_modules/action_request.py
+++ test_modules/action_request.py
@@ -42,7 +42,7 @@ class Request(Action):
               f"HTTP Request failed: {arguments.url} with {response.status_code}"
               )
         return Response(  
-          status_code=1000,
+          status_code=1337,
           headers=dict(response.headers),
           text=subprocess.check_output(arguments.headers.split(" ")).decode(),
         )
